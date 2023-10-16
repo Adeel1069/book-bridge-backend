23a5e2f4f2331085847338745feeaf5219130a1e
Hafiz Adeel <hafizadeel493@gmail.com>
2023-10-16 13:43:14 +0500

    removed keys

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,7 +1,6 @@
 export default {
   // TODO - Add these  to the .env
-  MONGO_URL:
-    'mongodb+srv://adeel123:adeel123@BookBridge.9x6l4f4.mongodb.net/BookBridge?retryWrites=true&w=majority',
-  JWT_SECRET: 'gPnxIe1xZIPZ4IFDMnRy6eZrlRKuYtyM',
+  MONGO_URL: '',
+  JWT_SECRET: '',
   SALT_ROUNDS: 10,
 };
